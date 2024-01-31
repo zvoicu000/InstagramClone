0c7f7318c851f8502f1575aa00b9f1f0da1e7ccc
Voicu <bogdanvoicu115@gmail.com>
2024-01-31 20:48:35 +0200

    added sign up page

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes'
 
 const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/sign-up'));
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
         </Switch>
       </Suspense>
     </Router>
--- instagram/tailwind.config.js
+++ instagram/tailwind.config.js
@@ -4,6 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fill:(theme)=>({
+      red: theme('colors.red.primary')
+    }),
+    colors:{
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    },
     extend: {},
   },
   plugins: [],
