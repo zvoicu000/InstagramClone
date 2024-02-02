5a2a4cccd9275dc419e8ab07dcf750b37ad2df7b
Voicu <bogdanvoicu115@gmail.com>
2024-02-02 18:56:48 +0200

    worked on header

--- instagram/src/pages/not-found.jsx
+++ instagram/src/pages/not-found.jsx
@@ -2,7 +2,7 @@ import {useEffect} from "react"
 
 export default function NotFound(){
     useEffect(()=>{
-        document.title='Not Found!';
+        document.title='Not Found - Instagram';
     },[])
     return(
         <div className="bg-gray-background">
