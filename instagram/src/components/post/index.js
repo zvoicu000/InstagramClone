bdd40ae7ac864574725d8f7e4d7c8759680ee69e
Voicu <bogdanvoicu115@gmail.com>
2024-03-19 19:45:46 +0200

    worked on comments etc

--- instagram/src/components/post/index.js
+++ instagram/src/components/post/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Header from "./header";
 import Image from './image'
 import Actions from './actions'
-
+import Footer from "./footer";
+import Comments from "./comments";
 export default function Post({content}){
     const commentInput=useRef(null)
     const handleFocus=()=> commentInput.current.focus();
@@ -19,6 +20,12 @@ export default function Post({content}){
             likedPhoto={content.userLikedPhoto}
             handleFocus={handleFocus}
             />
+        <Footer caption={content.caption} username={content.username}></Footer>
+        <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}/>
     </div>
     )
 }
